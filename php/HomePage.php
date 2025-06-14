058aee43a9cae49e1ee6f3ff97e543ca782b8a92
Arian <arian.shafagh2003@gmail.com>
2025-06-14 18:30:59 +0200

    1 version

---
+++ php/HomePage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HomePage</title>
+</head>
+<body>
+    <h1>Welcome to the Home Page</h1>
+    <a href="SQL.php">Open SQL Page</a>
+    <a href="MongoDB.php">Open MongoDB Page</a>
+    <a href="Neo4j.php">Open Neo4j Page</a>
+</body>
+</html>
